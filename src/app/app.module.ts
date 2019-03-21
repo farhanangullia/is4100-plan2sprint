35c20b07c8a49862cf5b595f821d8d16f9716dfb
wardzxzxc <wardzxzxc@gmail.com>
2019-03-21 23:36:47 +0800

    Initial Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from "./material.module";
+import { SidenavComponent } from './sidenav/sidenav.component';
 import { AppComponent } from './app.component';
+import { Browser } from 'protractor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
