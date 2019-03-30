44630aee35c7370159a384d9577637def06fa72c
Farhan <farhanangullia@gmail.com>
2019-03-30 15:45:27 +0800

    Created user stories data picker with UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from "./material.module";
@@ -9,6 +10,18 @@ import { SidenavComponent } from './shared/sidenav/sidenav.component';
 import { AppComponent } from './app.component';
 import { Browser } from 'protractor';
 import { UserStoriesComponent } from './pages/user-stories/user-stories.component';
+
+import { PickListModule } from 'primeng/picklist';
+import { DataGridModule, DataGrid } from 'primeng/datagrid';
+import { DialogModule } from 'primeng/dialog';
+import { DataViewModule } from 'primeng/dataview';
+import { PanelModule } from 'primeng/primeng';
+import { TabViewModule } from 'primeng/primeng';
+import { CodeHighlighterModule } from 'primeng/primeng';
+import { CheckboxModule } from 'primeng/primeng';
+import { ToggleButtonModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -22,7 +35,19 @@ import { UserStoriesComponent } from './pages/user-stories/user-stories.componen
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModule,
+    PickListModule,
+    TabViewModule,
+    CodeHighlighterModule,
+    DataGridModule,
+    PanelModule,
+    DialogModule,
+    DataViewModule,
+    CheckboxModule,
+    ToggleButtonModule,
+    DropdownModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/user-stories/user-stories.component.ts
+++ src/app/pages/user-stories/user-stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as project1Data from '../../../data/project1_onesprint.json'
 
 @Component({
   selector: 'app-user-stories',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserStoriesComponent implements OnInit {
 
+  userStories: any[];
+
+  targetUserstories: any[];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(<any>project1Data.title);
+    this.userStories = project1Data.sprints[0].userStories;
+    console.log(this.userStories);
+    this.targetUserstories = [];
   }
 
 }
